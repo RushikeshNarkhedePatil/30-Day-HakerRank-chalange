8b73d090cc66196c972450f644d85ae93d80f11c
Rushikesh Narkhede <61877894+RushikeshNarkhedePatil@users.noreply.github.com>
2022-03-10 16:49:26 +0530

    update day 1 code

--- Day 1_Data_Type.c
+++ Day 1_Data_Type.c
@@ -7,10 +7,13 @@ int main() {
     int i = 4;
     double d = 4.0;
     char s[] = "HackerRank ";
-
-   printf("%d\n",i+12);
-   printf("%0.1lf\n",d+4);
-   printf("%sis the best place to learn and practice coding!",s);
+    int iValue=0;
+    double iValue2=0.0;
+    char str[100];
+    scanf("%d %lf %[^\n]",&iValue,&iValue2,str);
+   printf("%d\n",i+iValue);
+   printf("%0.1lf\n",d+iValue2);
+   printf("%s%s",s,str);
    
     return 0;
 }
