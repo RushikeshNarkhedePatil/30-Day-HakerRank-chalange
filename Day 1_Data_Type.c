2ee4caf69e3a2b68d3159a6fea680d34f4edd964
Rushikesh Narkhede <61877894+RushikeshNarkhedePatil@users.noreply.github.com>
2022-03-10 16:23:11 +0530

    Create Day 1_Data_Type.c

---
+++ Day 1_Data_Type.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    char s[] = "HackerRank ";
+
+   printf("%d\n",i+12);
+   printf("%0.1lf\n",d+4);
+   printf("%sis the best place to learn and practice coding!",s);
+   
+    return 0;
+}
